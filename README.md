cc45b618d8130b1e021e44c249a8ead5a34cc21f
Apple <tapple@takahashiyuukinoMacBook-Air.local>
2021-02-03 19:34:12 +0900

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # ApplicationKotlin
+# ApplicationKotlin
+# app
---
+++ app/src/main/java/com/example/applicationkotlin/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.example.applicationkotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        var helloText : TextView = findViewById(R.id.textView)
+        var helloButton : Button = findViewById(R.id.tapHere)
+        helloButton.setOnClickListener{
+            helloText.text = "押した"
+        }
+
+    }
+}
