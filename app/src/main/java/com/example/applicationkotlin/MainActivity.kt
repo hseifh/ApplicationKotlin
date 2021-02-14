67f344655aca78b8ee4f861108784f0d115f2da1
tapple <hseifhegruh@gmail.com>
2021-02-15 01:53:34 +0900

    24nd commit

--- app/src/main/java/com/example/applicationkotlin/MainActivity.kt
+++ app/src/main/java/com/example/applicationkotlin/MainActivity.kt
@@ -9,8 +9,7 @@ import com.example.applicationkotlin.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
-
-
+        
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
