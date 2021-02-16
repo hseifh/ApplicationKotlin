3876991554d0f3d02827474dd6ee8f33b7e7a9a6
tapple <hseifhegruh@gmail.com>
2021-02-17 02:00:01 +0900

    24nd commit

--- app/src/main/java/com/example/applicationkotlin/MainActivity.kt
+++ app/src/main/java/com/example/applicationkotlin/MainActivity.kt
@@ -15,7 +15,6 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-
         
         /*
         super.onCreate(savedInstanceState)
