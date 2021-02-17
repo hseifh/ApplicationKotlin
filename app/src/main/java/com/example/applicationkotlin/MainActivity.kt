bd95a8743fd7bf9a69fbe036f61e383bb0793351
tapple <hseifhegruh@gmail.com>
2021-02-18 01:15:10 +0900

    24nd commit

--- app/src/main/java/com/example/applicationkotlin/MainActivity.kt
+++ app/src/main/java/com/example/applicationkotlin/MainActivity.kt
@@ -19,6 +19,19 @@ class MainActivity : AppCompatActivity() {
         binding.choki.setOnClickListener{ onJankenButtonTapped(it) }
         binding.pa.setOnClickListener{ onJankenButtonTapped(it) }
 
+
+
+
+
+
+        binding.tapHere.setOnClickListener {
+            //binding.textView.text = "ボタンがタップされました"
+            //println("ランダムクラス")
+            //println("Math.random()" + Math.random())
+            //println("(Math.random() * 3)" + (Math.random() * 3))
+            println("(Math.random() * 3).toInt()" + (Math.random() * 3).toInt())
+        }
+
     }
 
     fun onJankenButtonTapped(view: View?){
@@ -33,31 +46,30 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         var view = binding.root
         setContentView(view)
-
-
         binding.tapHere.setOnClickListener {
             binding.textView.text = "ボタンがタップされました"
         }
         */
 
 
-        /*
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        println("seihfgisehgfiruwheureur")
-        var helloText: TextView = findViewById(R.id.textView)
-        var helloButton: Button = findViewById(R.id.tapHere)
-        helloButton.setOnClickListener {
-            if (helloText.text == "push️") {
-                helloText.text = "押した"
-                println("押した")
-                //for (i in 0..200) println(i)
-            } else {
-                helloText.text   = "push️"
-                println("push")
-            }
-        }
-        */
 
-    //}
+    /*
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_main)
+    println("seihfgisehgfiruwheureur")
+
+    var helloText: TextView = findViewById(R.id.textView)
+    var helloButton: Button = findViewById(R.id.tapHere)
+
+    helloButton.setOnClickListener {
+        if (helloText.text == "push️") {
+            helloText.text = "押した"
+            println("押した")
+            //for (i in 0..200) println(i)
+        } else {
+            helloText.text   = "push️"
+            println("push")
+        }
+    }
+    */
 }
