94a2000d0b298fc350a2a2daaaf1c2af6cbdcf8e
tapple <hseifhegruh@gmail.com>
2021-02-17 21:19:30 +0900

    24nd commit

--- app/src/main/java/com/example/applicationkotlin/MainActivity.kt
+++ app/src/main/java/com/example/applicationkotlin/MainActivity.kt
@@ -9,14 +9,15 @@ import android.widget.TextView
 import com.example.applicationkotlin.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-
     private lateinit var binding: ActivityMainBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
-
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.gu.setOnClickListener { onJankenButtonTapped(it) }
+        binding.choki.setOnClickListener{ onJankenButtonTapped(it) }
+        binding.pa.setOnClickListener{ onJankenButtonTapped(it) }
 
     }
 
